14abec51a74fb0ed3f20e46978c938d0dd4e9761
Alejandro <alextalin@gmail.com>
2021-05-26 21:36:46 +0200

    Added navigation stack to project

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import React, { useState, useEffect } from 'react';
 import { BarCodeScanner } from 'expo-barcode-scanner';
 import { Camera } from 'expo-camera';
 
-export default function App() {
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
+    </View>
+  );
+}
+
+function DetailsScreen({ navigation }) {
 
   const [hasPermission, setHasPermission] = useState(null);
   const [scanned, setScanned] = useState(false);
@@ -28,25 +41,38 @@ export default function App() {
     alert(`Bar code with type ${type} and data ${data} has been scanned!`);
   };
 
-
   return (
     <View style={styles.container}>
-      <Camera style={styles.camera}
-          barCodeScannerSettings={{barCodeTypes: [BarCodeScanner.Constants.BarCodeType.ean13],}}
-          onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
-      >
-        <View style={styles.buttonContainer}>
-          <TouchableOpacity
-            style={styles.button}
-          >
-            <Text style={styles.text}> Volver </Text>
-          </TouchableOpacity>
-        </View>
-      </Camera>
-      {scanned && <Button title={'Volver a escanear'} onPress={() => setScanned(false)} />}
-    </View>
+    <Camera style={styles.camera}
+        barCodeScannerSettings={{barCodeTypes: [BarCodeScanner.Constants.BarCodeType.ean13],}}
+        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+    >
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity
+          style={styles.button}
+        >
+          <Text style={styles.text}> Volver </Text>
+        </TouchableOpacity>
+      </View>
+    </Camera>
+    {scanned && <Button title={'Volver a escanear'} onPress={() => setScanned(false)} />}
+  </View>
   );
 }
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
 
 const styles = StyleSheet.create({
   container: {
@@ -70,3 +96,5 @@ const styles = StyleSheet.create({
     color: 'white',
   },
 });
+
+export default App;
